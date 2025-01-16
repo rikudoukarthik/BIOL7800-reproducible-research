93851a6c38ee7278377b598680dbd1c239ddf3ce
rikudoukarthik <rikudoukarthik@gmail.com>
2025-01-16 14:35:17 -0600

    sampling for coconuts

--- weekly-notes/w02_class2.R
+++ weekly-notes/w02_class2.R
@@ -10,3 +10,8 @@ ggplot(iris) +
   geom_point(mapping = aes(x = Species, y = Sepal.Length, col = Species), 
              alpha = 0.75, position = position_jitter(height = 0, width = 0.25)) +
   theme_classic()
+
+
+set.seed(123)
+sample(c("apples", "bananas", "dacoconutnutisagiantnut"), 5, TRUE)
+
