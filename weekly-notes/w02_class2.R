1d96f846e57af8478c62d34079b3440100beecc8
rikudoukarthik <rikudoukarthik@gmail.com>
2025-01-16 14:32:29 -0600

    added some code

--- weekly-notes/w02_class2.R
+++ weekly-notes/w02_class2.R
@@ -1,2 +1,12 @@
 1:10 
 sum(1:10)
+
+
+print("To git or not to git")
+
+
+library(tidyverse)
+ggplot(iris) +
+  geom_point(mapping = aes(x = Species, y = Sepal.Length, col = Species), 
+             alpha = 0.75, position = position_jitter(height = 0, width = 0.25)) +
+  theme_classic()
